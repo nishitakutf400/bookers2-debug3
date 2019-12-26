85336df6318def1c83aff651073e4b81a2f14f38
yasan <track400m47@gmail.com>
2019-12-26 05:11:02 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,:validatable
 
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
   has_many :books
   attachment :profile_image, destroy: false
 
---
+++ db/migrate/20191224091957_rename_post_image_id_column_to_book_id.rb
@@ -0,0 +1,5 @@
+class RenamePostImageIdColumnToBookId < ActiveRecord::Migration[5.2]
+  def change
+  	rename_column :favorites, :post_image_id, :book_id
+  end
+end
